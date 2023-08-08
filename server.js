98c2eb721ae353de64f8dd2ce5a085e81712e33a
AshtonJ7 <AshtonJ7@users.noreply.github.com>
2023-08-08 18:41:06 +0100

    hi

--- server.js
+++ server.js
@@ -42,5 +42,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
+    console.log("Server is running.");
+  })
 });

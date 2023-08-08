acc7afad3d0c52378dd96da5550c5c957a4aed9b
AshtonJ7 <AshtonJ7@users.noreply.github.com>
2023-08-08 23:29:37 +0100

    start

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,30 +3,6 @@ const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
-    console.log('======================');
-    Post.findAll({
-            attributes: ['id',
-                'title',
-                'content',
-                'created_at'
-            ],
-            order: [
-                ['created_at', 'DESC']
-            ],
-            include: [{
-                    model: User,
-                    attributes: ['username']
-                },
-            ]
-        })
-        .then(dbPostData => res.json(dbPostData.reverse()))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-
-});
 router.get('/:id', (req, res) => {
     Post.findOne({
             where: {
